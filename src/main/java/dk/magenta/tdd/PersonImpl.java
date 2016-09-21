8fd58ae4eeaa7db7f5be07d1564a8a5a63f48ea2
Andreas Kring <andreas@magenta.dk>
2016-09-21 21:07:00 +0200

    First test case added

---
+++ src/main/java/dk/magenta/tdd/PersonImpl.java
@@ -0,0 +1,24 @@
+package dk.magenta.tdd;
+
+public class PersonImpl implements Person {
+
+	public float getMass() {
+		return 50;
+	}
+
+	public void setMass(float mass) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public float getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getBMI() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/test/java/dk/magenta/tdd/TestPerson.java
+++ src/test/java/dk/magenta/tdd/TestPerson.java
@@ -1,12 +1,23 @@
 package dk.magenta.tdd;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestPerson {
 
 	// height should be 1.78 if the person is 1.78 cm height
 	// height should be 1.85 if the person is 1.85 cm height
-	// person weighing 50 kg should have a mass of 50 kg
+
 	// person weighing 75 kg should have a mass of 75 kg
 	// person cannot have negative mass
 	// a 2 m height person weighing 100 kg should have a BMI = 25
+
+
+	@Test
+	public void shouldReturnMassOf50ForAPersonWeighing50kg() {
+		Person person = new PersonImpl();
+		assertEquals(50, person.getMass(), 1e-7);
+	}
 	
 }
