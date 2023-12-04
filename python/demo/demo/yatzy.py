4aca200be0a164a132cdff4f2a6eb8b9fd0f55ee
Andreas Kring <andreas@magenta-aps.dk>
2023-12-04 15:39:37 +0100

    Initial Python Yatzy commit

---
+++ python/demo/demo/yatzy.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+
+class Category(Enum):
+    ONES = "ones"
+    TWOS = "twos"
+    # ...
+    FIVES = "fives"
+    # ...
+    CHANCE = "chance"  # Sum of dices
+    YATZY = "yatzy"  # All dices identical
+    FULL_HOUSE = "full_house"
+
+
+def score(category: Category, dices: tuple[int, ...]) -> int:
+    """
+    Calculate the score of a dice roll with a given category
+    """
+    pass
+
+
+def score_twos(dices: tuple[int, ...]) -> int:
+    """
+    Calculate the score of the dice roll within the TWOS category
+    """
+    pass
+
+
+def score_fives(dices: tuple[int, ...]) -> int:
+    """
+    Calculate the score of the dice roll within the FIVES category
+    """
+    pass
+
+
+def score_chance(dices: tuple[int, ...]) -> int:
+    """
+    Calculate the score of the dice roll within the CHANCE category
+    """
+    pass
+
+
+def score_yatzy(dices: tuple[int, ...]) -> int:
+    """
+    Calculate the score of the dice roll within the YATZY category
+    """
+    pass
---
+++ python/demo/tests/test_yatzy.py
@@ -0,0 +1,13 @@
+# The roll 2,2,3,2,5 within the category TWOS should score 6
+# The roll 2,2,3,2,2 within the category TWOS should score 8
+# The roll 3,4,3,5,5 within the category TWOS should score 0
+# The roll 3,4,3,5,5 within the category FIVES should score 10
+
+# The roll 3,4,3,5,5 within the category CHANCE should score 20
+# The roll 1,2,3,4,4 within the category CHANCE should score 14
+
+# The roll 3,3,3,3,3 within the category YATZY should score 50
+# The roll 1,3,3,3,3 within the category YATZY should score 0
+
+# If the first dice is 0, a ValueError should be raised
+# If the second dice is 7, a ValueError should be raised
